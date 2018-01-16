995fd04cc5ca45debd9d7650a85810e724fd6577
Yichao Zhou <broken.zhou@gmail.com>
2018-01-15 19:53:23 -0800

    first commit

---
+++ record.py
@@ -0,0 +1,45 @@
+import pyaudio
+import wave
+import numpy as np
+import speech
+import timeit
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 2
+WAVE_OUTPUT_FILENAME = "output.wav"
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+print("* recording")
+
+frames = []
+
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    decoded = np.fromstring(data, dtype=np.int16)
+    frames.append(decoded)
+
+print("* done recording")
+
+F = np.concatenate(frames)
+speech.mfcc(F, RATE)
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
